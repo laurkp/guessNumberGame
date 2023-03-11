4ee8907374b2277e65941aba60f938ac8e0f4455
laurkp <laurentziukp@gmail.com>
2023-03-11 22:38:10 +0000

    Separating the code into classesAdd project files.

---
+++ guessNumberGame/GuessNumberMethods.cs
@@ -0,0 +1,80 @@
+﻿
+namespace guessNumberGame
+{
+    public static class GuessNumberMethods
+    {
+        public static int guessLimit = 5;
+        public static int guessCounter = 0;
+        /// <summary>
+        /// Return the initial number of guesses 
+        /// </summary>
+        /// <returns></returns>
+        public static int Limit()
+        {
+            int limit = guessLimit;
+            return limit;
+        }
+        /// <summary>
+        /// Returns the initial number of tries
+        /// </summary>
+        /// <returns></returns>
+        public static int Counter()
+        {
+            int counter = guessCounter;
+            return counter;
+        }
+        /// <summary>
+        /// Prompting that the maximum number of guesses has been reached
+        /// </summary>
+        public static void YouLose()
+        {
+            Console.WriteLine("You have reached the maximum number of guesses.\nYou lose");
+        }
+        /// <summary>
+        /// Prompting the number of guesses left
+        /// </summary>
+        /// <param name="result"></param>
+        public static void RemainingGuesses(int result)
+        {
+            Console.WriteLine($"You have {result} guesses remaining");
+        }
+        /// <summary>
+        /// Ask for a user input
+        /// </summary>
+        /// <returns></returns>
+        public static int UserInput()
+        {
+            Console.Write("Please guess a number between 1 and 100: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            return userInput;
+        }
+        /// <summary>
+        /// Prompt if the number choosen is to slow
+        /// </summary>
+        public static void ToLow()
+        {
+            Console.WriteLine("The number you choose is too low, try again");
+        }
+        /// <summary>
+        /// Prompt if the number choosen is to high
+        /// </summary>
+        public static void ToHigh()
+        {
+            Console.WriteLine("The number you choose is too high, try again");
+        }
+        /// <summary>
+        /// Prompt if the number choosen is within the range of 5 numbers from the random number
+        /// </summary>
+        public static void Almost()
+        {
+            Console.WriteLine("You're close!");
+        }
+        /// <summary>
+        /// Prompt if the number choosen is the same like the random number
+        /// </summary>
+        public static void Win()
+        {
+            Console.WriteLine("Congratulation you won!!! Lets try again");
+        }
+    }
+}
---
+++ guessNumberGame/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace guessNumberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random nmb = new Random();
+
+            while (true)
+            {
+                // Chosing a random number     
+                int randomNumber = nmb.Next(1, 101);
+
+                int tryCounter = GuessNumberMethods.Counter();
+                int tryLimit = GuessNumberMethods.Limit();
+
+                while (true)
+                {
+                    // Checking if the user has made 5 guesses
+                    if (tryCounter == tryLimit)
+                    {
+                        GuessNumberMethods.YouLose();
+                        break;
+                    }
+
+                    // Showing the number of remaining guesses
+                    int remainingGuesses = tryLimit - tryCounter;
+                    GuessNumberMethods.RemainingGuesses(remainingGuesses);
+
+                    // Asking the user to guess the number 
+                    int numberGuess = GuessNumberMethods.UserInput();
+
+                    // Using  if statements to check if the numbers are to high, to low or the same 
+                    if (numberGuess > randomNumber)
+                    {
+                        GuessNumberMethods.ToHigh();
+                    }
+                    if (numberGuess < randomNumber)
+                    {
+                        GuessNumberMethods.ToLow();
+                    }
+
+                    // Checking if the guess is within 5 of the random number
+                    if (Math.Abs(numberGuess - randomNumber) <= 5)
+                    {
+                        GuessNumberMethods.Almost();
+                    }
+                    if (numberGuess == randomNumber)
+                    {
+                        GuessNumberMethods.Win();
+                        break;
+                    }
+
+                    // Increment the guess counter
+                    tryCounter++;
+                }
+            }
+        }
+    }
+}
