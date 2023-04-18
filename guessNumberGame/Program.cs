a7522c8b53624a949c2b2b6a105256ab93d8e8d5
laurkp <120257523+laurkp@users.noreply.github.com>
2023-04-18 20:57:54 +0100

    Deleting methods

--- guessNumberGame/Program.cs
+++ guessNumberGame/Program.cs
@@ -1,60 +1,60 @@
-﻿
-
 namespace guessNumberGame
 {
     internal class Program
     {
+        public const int GUESS_LIMIT = 5;
         static void Main(string[] args)
         {
             Random nmb = new Random();
 
             while (true)
             {
+                // Initialize guess counter to 0
+                int guessCounter = 0;
+
                 // Chosing a random number
                 int randomNumber = nmb.Next(1, 101);
-
-                int tryCounter = GuessNumberMethods.Counter();
-                int tryLimit = GuessNumberMethods.Limit();
 
                 while (true)
                 {
                     // Checking if the user has made 5 guesses
-                    if (tryCounter == tryLimit)
+                    if (guessCounter == GUESS_LIMIT)
                     {
-                        GuessNumberMethods.YouLose();
+                        Console.WriteLine("You have reached the maximum number of guesses.\nYou lose");
                         break;
                     }
 
                     // Showing the number of remaining guesses
-                    int remainingGuesses = tryLimit - tryCounter;
-                    GuessNumberMethods.RemainingGuesses(remainingGuesses);
+                    int remainingGuesses = GUESS_LIMIT - guessCounter;
+                    Console.WriteLine($"You have {remainingGuesses} guesses remaining");
 
                     // Asking the user to guess the number 
-                    int numberGuess = GuessNumberMethods.UserInput();
+                    Console.Write("Please guess a number between 1 and 100: ");
+                    string userInput = Console.ReadLine();
+                    int numberGuess = Convert.ToInt32(userInput);
 
                     // Using  if statements to check if the numbers are to high, to low or the same 
                     if (numberGuess > randomNumber)
                     {
-                        GuessNumberMethods.ToHigh();
+                        Console.WriteLine("The number you choose is too high, try again");
                     }
                     if (numberGuess < randomNumber)
                     {
-                        GuessNumberMethods.ToLow();
+                        Console.WriteLine("The number you choose is too low, try again");
                     }
-
                     // Checking if the guess is within 5 of the random number
                      if (Math.Abs(numberGuess - randomNumber) <= 5)
                     {
-                        GuessNumberMethods.Almost();
+                        Console.WriteLine("You're close!");
                     }
                     if (numberGuess == randomNumber)
                     {
-                        GuessNumberMethods.Win();
+                        Console.WriteLine("Congratulation you won!!! Lets try again");
                         break;
                     }
 
                     // Increment the guess counter
-                    tryCounter++;
+                    guessCounter++;
                 }
             }
         }
